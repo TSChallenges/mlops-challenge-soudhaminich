55ceca57030918bb7db4940ddda606c575d340c5
Soudhamini Cheethirala <soudhamini999@gmail.com>
2024-11-12 19:49:40 +0000

    checking feature names

--- src/run_model.py
+++ src/run_model.py
@@ -9,15 +9,23 @@ sys.path.append(str(root))
 
 def load_model():
     
-    model_path = '/models/churn_model.pkl' 
+    model_path = '/workspaces/mlops-challenge-soudhaminich/models/churn_model.pkl' 
     with open(model_path, 'rb') as model_file:
         model = pickle.load(model_file)
     return model
 
 def get_customer_input():
-    credit_score = float(input("Enter credit score: "))
     # ENTER YOUR CODE HERE
-    return [[credit_score]]
+    credit_score = float(input("Enter credit score: "))
+    age = int(input("Enter age: "))
+    balance = float(input("Enter balance: "))
+    products_number = int(input("Enter product_number: "))
+    credit_card = int(input("Enter 0/1 if credit card is there 1 or 0 if no: "))
+    active_member = int(input("Enter 0/1 if active member 1 or 0 if no: "))
+    estimated_salary = float(input("Enter age: "))
+    tenure = int(input("Enter tenure: "))
+    
+    return [[credit_score, age, balance, products_number, credit_card, active_member, estimated_salary, tenure]]
 
 def predict_churn(model, customer_data):
     prediction = model.predict(customer_data)
